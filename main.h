87b5a080a5d303e0f55ba7f94f1b441cf659beba
tony ngundungu <anthonymsalami@gmail.com>
2023-01-26 11:22:52 +0300

    commit

--- main.h
+++ main.h
@@ -1,10 +1,31 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-int _printf(const char *format, ...);
-int _putchar(char c);
-int loopstr(const char *str);
-void print_num(int n);
-int _puts(char *p);
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
+
+
+int _printf(const char *format, ...);
+char *_strcat(char *dest, char *src, int n);
+
+
+/**
+ * struct type - Struct data type
+ *
+ * @op: data type argument
+ * @f: The function associated
+ */
+
+typedef struct type
+{
+	char *op;
+	char *(*f)(va_list);
+
+
+} type_t;
+char *print_c(va_list args);
+char *print_s(va_list args);
 #endif
