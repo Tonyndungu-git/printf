9afad9421dd8985c35af2ce9fa47f62cf0123ab3
tony ngundungu <anthonymsalami@gmail.com>
2023-01-26 12:01:58 +0300

    commit

--- printf.c
+++ printf.c
@@ -67,26 +67,3 @@ int _printf(const char *format, ...)
 	free(create_buff);
 	return (len_buf);
 }
-
-#include <limits.h>
-#include <stdio.h>
-#include "main.h"
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-
-     _printf("Let's try to printf a simple sentence.\n");
-     printf("Let's try to printf a simple sentence.\n");
-     _printf("Character:[%c]\n", 'H');
-     printf("Character:[%c]\n", 'H');
-     _printf("String:[%s]\n", "I am a string !");
-     printf("String:[%s]\n", "I am a string !");
-     _printf("Percent:[%%]\n");
-     printf("Percent:[%%]\n");
-    return (0);
-}
